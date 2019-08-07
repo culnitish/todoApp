a51fa6d51e54fcbf8ceffc04f0a47d083a031c4b
dhawal.sonawane@dev.figmd.in <dhawal.sonawane@figmd.com>
2019-08-07 15:37:18 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-s
+const express = require('express');
+// for Creating a Server
+const app = express();
+
+app.get('/api',(req,res)=>{
+    res.send("TO DO APP");
+    console.log("IN TO DO APPP");
+});
+app.listen(3000);
+console.log('App Listening on port 3000');
