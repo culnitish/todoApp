ec682c6468059d5d12eaa3aef2fcf958c87fb073
dhawal.sonawane@dev.figmd.in <dhawal.sonawane@figmd.com>
2019-08-07 18:01:14 +0530

    intial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // for Creating a Server
 const app = express();
 
-app.get('/api',(req,res)=>{
+app.get('/api/posts',(req,res)=>{
     res.send("TO DO APP");
     console.log("IN TO DO APP , Control Flow... ");
 });
