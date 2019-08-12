6753dbd8c6c99968f5f68277bd480d6f4a198714
dhawal.sonawane@dev.figmd.in <dhawal.sonawane@figmd.com>
2019-08-12 11:31:23 +0530

    database and route created Successfully with mvc changes

--- modules/todo/routes/todoRoute.js
+++ modules/todo/routes/todoRoute.js
@@ -1,6 +1,6 @@
 const todoController =  require("../controllers/todoController");
 const todoValidator  =  require("../validators/todoValidators");
-
+const express = require("express");
 //require('express-async-await')
 //const pg=require('pg');
 const router = express.Router();
